fc1010b2dea5fe1f4b6e7b4564ea3ac2e6156087
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-18 15:03:03 +0700

    feat: add contact section

---
+++ src/data/ContactData.js
@@ -0,0 +1,25 @@
+import LinkedIn from '../assets/LinkedIn.png';
+import Email from '../assets/Mail.png';
+import Call from '../assets/Call.png';
+import Wa from '../assets/Wa.png';
+
+export const ContactData = [
+    {
+        id: 1,
+        name: 'Email',
+        logo: Email,
+        link: 'mailto:ramherdianto@gmail.com',
+    },
+    {
+        id: 2,
+        name: 'LinkedIn',
+        logo: LinkedIn,
+        link: 'https://www.linkedin.com/in/ramadhanherdianto/',
+    },
+    {
+        id: 3,
+        name: 'Whatsapp',
+        logo: Wa,
+        link: 'https://wa.me/6282299231721?text=Hello%2C%20there%21',
+    },
+];
